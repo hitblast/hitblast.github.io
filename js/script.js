9ff370257aeb75a0ff5cb2fe0b31d7bf8d314e1c
HitBlast <hitblastlive@gmail.com>
2025-01-12 12:25:40 +0600

    updated static files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal("body", { delay: 200 });
+ScrollReveal().reveal("header", { delay: 400 });
+ScrollReveal().reveal("#top", { delay: 600 });
+ScrollReveal().reveal("#summary", { delay: 800 });
+ScrollReveal().reveal("footer", { delay: 200 });
