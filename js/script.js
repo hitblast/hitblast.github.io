c46c2666af6c860ed2779469efa95ca8c2430abf
HitBlast <hitblastlive@gmail.com>
2025-01-25 15:45:21 +0600

    updated basic front

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 ScrollReveal().reveal("body", { delay: 200 });
-ScrollReveal().reveal("header", { delay: 400 });
-ScrollReveal().reveal("#top", { delay: 600 });
-ScrollReveal().reveal("#summary", { delay: 800 });
+ScrollReveal().reveal("header", { delay: 500 });
+ScrollReveal().reveal("#top", { delay: 700 });
+ScrollReveal().reveal("#summary", { delay: 900 });
+ScrollReveal().reveal(".highlight-left", { delay: 200 });
+ScrollReveal().reveal(".highlight-right", { delay: 400 });
 ScrollReveal().reveal("footer", { delay: 200 });
