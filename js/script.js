ce556e5bd7f02a91111815bff957748a15b4848a
HitBlast <hitblastlive@gmail.com>
2025-05-17 16:42:53 +0600

    updated filse

--- js/script.js
+++
@@ -1,7 +0,0 @@
-ScrollReveal().reveal("body", { delay: 200 });
-ScrollReveal().reveal("header", { delay: 500 });
-ScrollReveal().reveal("#top", { delay: 700 });
-ScrollReveal().reveal("#summary", { delay: 900 });
-ScrollReveal().reveal(".highlight-left", { delay: 200 });
-ScrollReveal().reveal(".highlight-right", { delay: 400 });
-ScrollReveal().reveal("footer", { delay: 200 });
